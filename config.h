e50de0bf46db5ff481879c8cbfa6752a17b2f95b
Jane42070 <jql1377219787@gmail.com>
2021-02-10 13:14:55 +0800

    second commit

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Monaco:size=14",
-	"JoyPixels:size=14",
+	"Operator Mono SSm Book:pixelsize=21",
+	"JoyPixels:pixelsize=21",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
