d4ae675437b222509ef7ec27fd4b087cf2f03567
Jane42070 <jql1377219787@gmail.com>
2023-02-19 13:35:31 +0800

    change font

--- config.h
+++ config.h
@@ -5,10 +5,11 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Terminus:pixelsize=16",
-	"Dotted Songti Circle:pixelsize=16",
-	"Operator Mono SSm Book:pixelsize=16",
-	"JoyPixels:pixelsize=16",
+	"Cascadia Mono:size=11"
+	"GoMono Nerd Font Mono:size=11",
+	"WenQuanYi Zen Hei Mono:size=11",
+	"Operator Mono SSm Book:size=11",
+	"joypixels:size=11",
 };
 static const char *prompt = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
